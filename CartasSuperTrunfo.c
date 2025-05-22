46c81b822b8983c5348d5ea553acbe8659720ed2
Rczerlin <rczerlin@gmail.com>
2025-05-22 02:11:44 +0000

    Super Trunfo Mestre 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,87 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
+char cidade01A[50], cidade02B[50];
+float populacao01A, populacao02B;
+float area01A, area02B;
+float pib01A, pib02B;
+int numero_pontos_turisticos01A, numero_pontos_turisticos02B;
+float densidadepopulacional01A, densidadepopulacional02B;
+float pibpercapita01A, pibpercapita02B;
+float superpodercarta01A, superpodercarta02B;
+
+
+printf("Super Trunfo! \n");
+
+printf("Carta Cidade 01A \n");
+
+printf("Digite a Populacao: \n");
+scanf("%f", &populacao01A);
+
+printf("Digite a Area: \n");
+scanf("%f", &area01A);
+
+printf("Digite o PIB: \n");
+scanf("%f", &pib01A);
+
+printf("Digite o numero_de_pontos_turisticos: \n");
+scanf("%d", &numero_pontos_turisticos01A);
+
+printf("Populacao: %.2f \n", populacao01A);
+printf("Area: %.2f \n", area01A);
+printf("PIB: %.2f \n", pib01A);
+printf("Numero de pontos turisticos: %d \n", numero_pontos_turisticos01A);
+
+densidadepopulacional01A = (populacao01A / area01A);
+printf("Densidade populacional: %.2f \n", densidadepopulacional01A);
+
+pibpercapita01A = (pib01A / populacao01A);
+printf("PIB per capita: %.2f \n", pibpercapita01A);
+
+superpodercarta01A = (densidadepopulacional01A + pibpercapita01A + numero_pontos_turisticos01A + pib01A + populacao01A + area01A);
+printf("Super poder da carta 01A: %.3f \n", superpodercarta01A);
+
+
+printf("\n");
+
+////////////////////////////////////////////////////////////////////////
+
+printf("\n");
+
+
+printf("Carta Cidade 02B \n");
+
+printf("Digite a Populacao: \n");
+scanf("%f", &populacao02B);
+
+printf("Digite a Area: \n");
+scanf("%f", &area02B);
+
+printf("Digite o PIB: \n");
+scanf("%f", &pib02B);
+
+printf("Digite o numero_de_pontos_turisticos: \n");
+scanf("%d", &numero_pontos_turisticos02B);
+
+printf("Populacao: %.2f \n", populacao02B);
+printf("Area: %.2f \n", area02B);
+printf("PIB: %.2f \n", pib02B);
+printf("Numero de pontos turisticos: %d \n", numero_pontos_turisticos02B);
+
+
+densidadepopulacional02B = (populacao02B / area02B);
+printf("Densidade populacional: %.2f \n", densidadepopulacional02B);
+
+pibpercapita02B = (pib02B / populacao02B);
+printf("PIB per capita: %.2f \n", pibpercapita02B);
+
+superpodercarta02B = (densidadepopulacional02B + pibpercapita02B + numero_pontos_turisticos02B + pib02B + populacao02B + area02B);
+printf("Super poder da carta 02B: %.3f \n", superpodercarta02B);
+
+
+return 0;
+
+
+
 }
